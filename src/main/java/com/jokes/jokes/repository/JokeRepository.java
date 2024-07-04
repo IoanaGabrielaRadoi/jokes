50941252e44432de27a63fe41a68da8cc558feb3
Ioana Gabriela Radoi <ioana.radoi92@gmail.com>
2024-07-04 11:15:39 +0300

    Delete src directory

--- src/main/java/com/jokes/jokes/repository/JokeRepository.java
+++
@@ -1,8 +0,0 @@
-package com.jokes.jokes.repository;
-
-
-import com.jokes.jokes.model.Joke;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface JokeRepository extends MongoRepository<Joke, Integer> {
-}
